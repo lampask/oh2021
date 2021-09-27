5bfbf0a2f7f7264874a78df2a877bfdee4385c6b
Adam Gramblička <adamgramblicka@hotmail.com>
2021-09-27 18:41:48 +0200

    QR Test

--- src/pages/q/[id].tsx
+++ src/pages/q/[id].tsx
@@ -3,27 +3,36 @@ import { useRouter } from "next/router";
 import {Meta} from "../../layout/Meta";
 import {Main} from "../../layout/Main";
 import {useSession} from "next-auth/client";
+import {Button} from "antd";
 
 const qr: React.FC = (props) => {
   const [session, loading] = useSession()
   const router = useRouter()
   const { id } = router.query
 
+  const activate = async() => {
+    if (navigator.geolocation) {
+      await navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude)
+        console.log(longitude)
+      }, error => {
+        console.log(error)
+      },{timeout:10000});
+    } else {
+      console.log("rip")
+    }
+  }
+
   let content = <></>
   if (loading) {
     content = <div>Loading...</div>
   } else {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        console.log(latitude)
-        console.log(longitude)
-      });
-    } else {
-      
-    }
-    content = <div>{id}</div>
+    content = <div>QR kód: {id}
+    {session ? <Button onClick={activate}>Aktivovať</Button> : null}
+    </div>
   }
+
 
   return (
     <Main
